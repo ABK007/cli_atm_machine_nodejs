b89c0ab1468a994f3f697580e9031370fafd2153
Muhammad Abubakar Khalid <31004320+ABK007@users.noreply.github.com>
2024-07-10 22:22:20 +0500

    added "user input code matching functionality"

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 import inquirer from "inquirer";
-
 let myBalance = 10000;
 let myPinCode = 4567;
-
-console.log("My current balance is: " + myBalance)
-
-
+console.log("My current balance is: " + myBalance);
 let enterPin = await inquirer.prompt([
     {
         name: "user_input_pincode",
         type: "number",
         message: "Enter your pin code: "
     }
-])
-
-
+]);
 if (enterPin.user_input_pincode === myPinCode) {
-    console.log("You pincode is correct. You have logged in successfully.")
-} else {
-    console.log("You pincode is incorrect. login attempt failed.")
+    console.log("You pincode is correct. You have logged in successfully.");
+}
+else {
+    console.log("You pincode is incorrect. login attempt failed.");
 }
