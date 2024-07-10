f01a670f508a39c08493f8d42817c7a088db8bec
Muhammad Abubakar Khalid <31004320+ABK007@users.noreply.github.com>
2024-07-10 22:59:15 +0500

    added functionality of withdraw and check balance

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import inquirer from "inquirer";
+import Choice from "inquirer/lib/objects/choice.js";
 
 let myBalance = 10000;
 let myPinCode = 4567;
@@ -17,6 +18,41 @@ let enterPin = await inquirer.prompt([
 
 if (enterPin.user_input_pincode === myPinCode) {
     console.log("You pincode is correct. You have logged in successfully.")
+
+    let operations = await inquirer.prompt([
+        {
+            name: "operations",
+            message: "Select an operation: ",
+            type: "list",
+            choices:[ { name: "Check balance", value: "check balance" }, // Use a different value for internal representation
+                        { name: "Withdraw", value: "withdraw" }],
+            
+        }
+    ])
+
+    if (operations.operations === "check balance") {
+        console.log("your current balance is: " + myBalance)
+    }
+    else if (operations.operations === "withdraw") {
+        
+        let enterAmount = await inquirer.prompt([
+            {
+                name: "withdraw_amount",
+                type: "number",
+                message: "Enter your amount: "
+            }
+        ])
+
+        if (enterAmount.withdraw_amount <= myBalance) {
+            myBalance = myBalance - enterAmount.withdraw_amount
+            console.log("Amount withdrawn. Your remaining balance is: " + myBalance)
+        } else {
+            console.log ("Please enter amount less than or equal to your current balance of " + myBalance)
+        }
+
+    }
+    
+
 } else {
     console.log("You pincode is incorrect. login attempt failed.")
 }
